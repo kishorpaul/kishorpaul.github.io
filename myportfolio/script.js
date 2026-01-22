b418be199978f8a5a77997fe3854831649412f40
R K C RAJAPPA <kishor.r.k.c.rajappa@accenture.com>
2026-01-22 12:17:02 +0530

    Changes for v 0.1

--- myportfolio/script.js
+++ myportfolio/script.js
@@ -5,3 +5,28 @@ menu_icon.onclick = () => {
     menu_icon.classList.toggle('bx-x')
     nav_bar.classList.toggle('active');
 }
+
+const hireBtn = document.getElementById("hire_btn");
+const popupForm = document.getElementById("popHire");
+const closeBtn = document.querySelector(".close");
+const form = popupForm.querySelector("form");
+
+hireBtn.onclick = () => {
+  popupForm.style.display = "block";
+};
+
+closeBtn.onclick = () => {
+  popupForm.style.display = "none";
+};
+
+window.onclick = (event) => {
+  if (event.target === popupForm) {
+    popupForm.style.display = "none";
+  }
+};
+
+form.onsubmit = (event) => { 
+    // event.preventDefault(); 
+    popupForm.style.display = "none";
+    alert("✅ Thank you! Your message has been sent.");
+}
